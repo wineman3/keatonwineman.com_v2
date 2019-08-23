cc91047d996d9d79d2635bb9d54a014517b0f3cf
Kwineman <kwineman@captechconsulting.com>
2019-08-23 15:38:11 -0400

    changed project title label

--- js/main.js
+++ js/main.js
@@ -5,8 +5,14 @@ $(document).ready(function() {
     $('#header').css('height', height);
 	   
 
-    //Scrollspy plugin
-    $('body').scrollspy({ target: '.navbar-spy'});
+    var img = new Image();
+    img.src = "/img/Wineman3_White-01.png";
+    img.style ="width: 30rem;"
+    img.class = "masthead-avatar mb-5";
+    img.onload=function(){$(img).fadeIn(1000);}
+    var header = document.getElementById('header');
+    img.style.display="none";
+    document.getElementById('avi-header').appendChild(img);
 
 
 });
